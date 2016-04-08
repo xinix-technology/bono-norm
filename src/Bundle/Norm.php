1c9c1d257540e8065353b32e01f90f084f92d83f
Ganesha <reekoheek@gmail.com>
2016-04-08 08:51:18 +0700

    finalized for app

---
+++ src/Bundle/Norm.php
@@ -0,0 +1,163 @@
+<?php
+namespace ROH\BonoNorm\Bundle;
+
+use LogicException;
+use RuntimeException;
+use InvalidArgumentException;
+use Bono\Bundle\Rest;
+use Bono\Http\Context;
+use Norm\Exception\FilterException;
+use ROH\Util\Inflector;
+
+class Norm extends Rest
+{
+    protected $collection;
+
+    public function __construct(array $options = [])
+    {
+        // detect collection if not specified
+        if (!isset($options['collection'])) {
+            $explodedUri = explode('/', $options['uri']);
+            $lastSegment = end($explodedUri);
+            $options['collection'] = Inflector::humanize($lastSegment);
+        }
+
+        $options['name'] = strtolower($options['collection']);
+
+        $this->addMiddleware(function (Context $context, $next) {
+            $context->depends('@norm');
+
+            // initialize collection
+            $this->getCollection($context);
+
+            if ($context['route.info'][0] === 1) {
+                $segments = explode('/', $context['route.info'][1]['handler'][1]);
+                $context['response.template'] = $this['name'] . '/' . end($segments);
+            }
+
+            try {
+                $next($context);
+            } catch ( FilterException $e) {
+                if (isset($context['@notification'])) {
+                    $errors = $e->getChildren();
+                    foreach ($errors as $error) {
+                        if (!($error instanceof FilterException)) {
+                            throw $error;
+                        }
+                        $context['@notification']->notify([
+                            'level' => 'error',
+                            'context' => $error->getContext(),
+                            'message' => $error->getMessage(),
+                        ]);
+                    }
+                }
+            }
+        });
+
+        parent::__construct($options);
+    }
+
+    public function search(Context $context)
+    {
+        $entries = $this->getCollection()->find();
+        return [
+            'entries' => $entries
+        ];
+    }
+
+    public function create(Context $context)
+    {
+        if ('POST' === $context->getMethod()) {
+            $entry = $this->getCollection()->newInstance();
+            $entry->set($context->getParsedBody());
+            $entry->save();
+
+            $context['@notification']->notify([
+                'level' => 'info',
+                'message' => 'Data created.',
+            ]);
+
+            return [
+                'entry' => $entry,
+            ];
+        }
+    }
+
+    public function update(Context $context)
+    {
+
+        $id = $context['id'];
+        if (!empty($id)) {
+            $entry = $entry = $this->getCollection()->findOne($id);
+        }
+
+        if (is_null($entry)) {
+            return $context->throwError(404);
+        }
+
+        if ('PUT' === $context->getMethod()) {
+            $entry->set($context->getParsedBody());
+            $entry->save();
+
+            $context['@notification']->notify([
+                'level' => 'info',
+                'message' => 'Data updated.',
+            ]);
+        }
+
+        return [
+            'entry' => $entry
+        ];
+    }
+
+    public function read(Context $context)
+    {
+        $id = $context['id'];
+        if (!empty($id)) {
+            $entry = $this->getCollection()->findOne($id);
+        }
+
+        if (is_null($entry)) {
+            return $context->throwError(404);
+        }
+
+        return [
+            'entry' => $entry
+        ];
+    }
+
+    public function delete(Context $context)
+    {
+        if ('DELETE' === $context->getMethod()) {
+            if (isset($context['id'])) {
+                $ids = [$context['id']];
+            }
+
+            foreach ($ids as $id) {
+                $entry = $this->getCollection()->findOne($context['id']);
+                if (isset($entry)) {
+                    $entry->remove();
+                }
+            }
+
+            $context['@notification']->notify([
+                'level' => 'info',
+                'message' => 'Data deleted.',
+            ]);
+        }
+    }
+
+    public function getCollection(Context $context = null)
+    {
+        if (is_null($this->collection)) {
+            $this->collection = $context['@norm']->factory($this['collection']);
+        }
+
+        return $this->collection;
+    }
+
+    public function getSchema()
+    {
+        return $this->collection->getSchema();
+    }
+}
--- src/Middleware/Norm.php
+++ src/Middleware/Norm.php
@@ -1,61 +1,60 @@
 <?php
-namespace ROH\FNorm\Middleware;
+namespace ROH\BonoNorm\Middleware;
 
-use Bono\App;
-use Bono\Http\Response;
-use ROH\Util\Options;
+use Bono\Http\Context;
 use ROH\Util\Thing;
-use Norm\Norm as TheNorm;
-use Norm\Filter\FilterException;
+use ROH\Util\Options;
+use Norm\Repository;
+use Norm\Filter;
+use Bono\App;
 
 class Norm
 {
-    public function __construct($options = array())
+    protected $options;
+
+    protected $repository;
+
+    public function __construct(array $options = [])
     {
-        $this->options = Options::create([
-        ])->merge($options);
-
-
-        if (!isset($this->options['datasources'])) {
-            throw new \Exception('[Norm] No data source configuration');
-        }
-
-        $datasources = $this->options['datasources'];
-
-        TheNorm::init($this->options);
-
-        foreach ($datasources as $name => $datasource) {
-            $datasource['config']['name'] = $name;
-
-            $meta = new Thing($datasource);
-
-            TheNorm::registerConnection($name, $meta->getHandler());
-        }
+        $this->options = Options::create([])->merge($options)->toArray();
     }
 
-    public function __invoke($request, $next)
+    public function getRepository(Context $context = null)
     {
-        // TODO set norm !include
-        // TODO set norm !tz
+        if (is_null($this->repository)) {
+            $this->repository = new Repository($this->options);
 
-        $response = $next($request);
-        if ($response->getError() instanceof FilterException) {
-            $response = $response->withStatus(400);
-            if (empty($request->getHeaderLine('response-content-type')) ||
-                $request->getHeaderLine('response-content-type') === 'text/html') {
-                foreach ($response->getError()->getChildren() as $ce) {
-                    $context = $ce instanceof FilterException ? $ce->context() : '';
-                    $request['$notification']->notify([
-                        'level' => 'error',
-                        'context' => $context,
-                        'code' => $ce->getCode(),
-                        'message' => $ce->getMessage(),
-                    ]);
-                }
-            } else {
-                $response = $response->withError($response->getError()->getChildren()[0]);
+            if (isset($context['response.renderer'])) {
+                $repository = $this->repository;
+                $this->repository->setRenderer(function ($template, $data) use ($context, $repository) {
+                    if ($context['response.renderer']->resolve($template)) {
+                        return $context['response.renderer']->render($template, $data);
+                    } else {
+                        return $repository->defaultRender($template, $data);
+                    }
+                });
             }
+            // $this->repository->setRenderer()
         }
-        return $response;
+        return $this->repository;
+    }
+
+    public function factory($collectionId, $connectionId = '')
+    {
+        return $this->repository->factory($collectionId, $connectionId);
+    }
+
+    public function __invoke(Context $context, $next)
+    {
+        // initiate repository
+        $repository = $this->getRepository($context);
+        if (isset($context['timezone'])) {
+            $repository->setAttribute('timezone', $context['timezone']);
+        }
+        // TODO set norm !include
+
+        $context['@norm'] = $this;
+
+        $next($context);
     }
 }
